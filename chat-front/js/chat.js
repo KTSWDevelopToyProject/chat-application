3ddcb50754fa8b21cac85588e1375ae26cc09804
10145349 <donghyeon.kim@kt.com>
2023-01-05 14:35:37 +0900

    기능 모두 구현

--- chat-front/js/chat.js
+++ chat-front/js/chat.js
@@ -1,45 +1,87 @@
-const eventSource = new EventSource("http://localhost:8080/sender/kdh/receiver/donghyeon");
+let userName = prompt("아이디를 입력하세요.");
+let roomNum = prompt("채팅방 번호를 입력하세요.");
+document.querySelector("#user_name").innerHTML = "<b>" + userName + "</b>";
+
+const eventSource = new EventSource(`http://localhost:8080/chat/room-number/${roomNum}`);
 
 eventSource.onmessage = (event) => {
     const data = JSON.parse(event.data);
-    console.log(2, data);
-    initMessage(data);
+    if(data.sender === userName) {
+        // 메시지 보낸 사람이 로그인 유저이면 파란색박스(우측)
+        initMyMessage(data);
+    } else {
+        // 그렇지 않으면 회색박스(좌측)
+        initYourMessage(data);
+    }
 }
 
-function getSendMsgBox(msg, datetime) {
+function getSendMsgBox(data) {
+
+    let md = data.createdAt.substring(5, 10);
+    let tm = data.createdAt.substring(11, 16);
+    convertedDateTime = tm + " | " + md;
+
     return `<div class="sent_msg">
-    <p>${msg}</p>
-    <span class="time_date"> ${datetime}</span>
+    <p>${data.msg}</p>
+    <span class="time_date"> ${convertedDateTime}       <b>${data.sender}</b></span>
+  </div>`;
+}
+
+function getReceiveMsgBox(data) {
+
+    let md = data.createdAt.substring(5, 10);
+    let tm = data.createdAt.substring(11, 16);
+    convertedDateTime = tm + " | " + md;
+
+    return `<div class="received_withd_msg">
+    <p>${data.msg}</p>
+    <span class="time_date"> ${convertedDateTime}       <b>${data.sender}</b></span>
   </div>`;
 }
 
 function addMessage() {
-    let chatBox = document.querySelector("#chat-box");
+   
     let msgInput = document.querySelector("#chat-outgoing-msg");
-    let chatOutgoingBox = document.createElement("div");
-    let date = new Date();
-    let now = date.getHours() + ":" + date.getMinutes() + " | " + date.getMonth() + "/" + date.getDate();
+    let chat = {
+        sender: userName,
+        roomNum: roomNum,
+        msg: msgInput.value,
+    }
 
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now);
-    chatOutgoingBox.className = "outgoing_msg";
+    fetch("http://localhost:8080/chat", {
+        method: "post",
+        body: JSON.stringify(chat),
+        headers: {
+            "Content-Type": "application/json;charset=utf-8"
+        },
+    });
 
-    chatBox.append(chatOutgoingBox);
     msgInput.value = "";
 }
 
-function initMessage(history) {
+// 파란 박스
+function initMyMessage(data) {
     let chatBox = document.querySelector("#chat-box");
-    let msgInput = document.querySelector("#chat-outgoing-msg");
-    let chatOutgoingBox = document.createElement("div");
+    let sendBox = document.createElement("div");
+    sendBox.className = "outgoing_msg";
     
-    chatOutgoingBox.innerHTML = getSendMsgBox(history.msg, history.createdAt);
-    chatOutgoingBox.className = "outgoing_msg";
+    sendBox.innerHTML = getSendMsgBox(data);
+    chatBox.append(sendBox);
 
-    chatBox.append(chatOutgoingBox);
-    msgInput.value = "";
+    document.documentElement.scrollTop = document.body.scrollHeight;
 }
 
+// 회색 박스
+function initYourMessage(data) {
+    let chatBox = document.querySelector("#chat-box");
+    let receivedBox = document.createElement("div");
+    receivedBox.className = "received_msg";
+    
+    receivedBox.innerHTML = getReceiveMsgBox(data);
+    chatBox.append(receivedBox);
 
+    document.documentElement.scrollTop = document.body.scrollHeight;
+}
 
 document.querySelector("#chat-send").addEventListener("click", () => {
     addMessage();
--- src/main/java/com/ktgame/chatapplication/ChatController.java
+++ src/main/java/com/ktgame/chatapplication/ChatController.java
@@ -15,10 +15,23 @@ public class ChatController {
 
     private final ChatRepository repository;
 
+    /**
+     * 귓속말 할 때 사용
+     */
     @CrossOrigin
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
         return repository.mFindBySender(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    /**
+     * 일반 챗 모두 조회
+     */
+    @CrossOrigin
+    @GetMapping(value = "/chat/room-number/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+        return repository.mFindByRoomNum(roomNum)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
